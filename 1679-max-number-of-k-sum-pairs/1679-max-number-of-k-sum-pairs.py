55c4465995a0f32d09b7f13588a9f9a837929d8a
Myk0-2716 <119573559+Myk0-2716@users.noreply.github.com>
2022-12-29 16:41:49 +0300

    Time: 735 ms (75.78%), Space: 26.3 MB (89.06%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -3,14 +3,16 @@ class Solution:
         start = 0
         end = len(nums)-1
         c = 0
-        nums = sorted(nums)
+        nums.sort()
         while start < end:
             if nums[start] + nums[end] > k:
                 end -=1
             elif nums[start] + nums[end] < k:
                 start +=1
             else:
-                c +=1
                 start +=1
                 end -=1
+                c +=1
+            
         return c
+                
